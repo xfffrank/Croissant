e0b120dfdb72c6f764def0df7be966c66ef10bba
Frank_Xie <xfffrank@gmail.com>
2017-11-04 21:31:38 +0800

    框架基本完成

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post, Category, Tag
+from markdownx.admin import MarkdownxModelAdmin
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+
+admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
---
+++ blog/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from markdownx.models import MarkdownxField
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    """
+    title：题目；
+    body：文章内容；
+    created_time：创建时间；
+    modified_time：最后一次修改时间；
+    author：作者；
+    category：博文分类；
+    tags：标签；
+    """
+    title = models.CharField(max_length=70)
+    # body = models.TextField()
+    body = MarkdownxField()
+
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
+
+
